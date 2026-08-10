5fbf2c8613476da3228370e37c20750519b13fb7
obedeDintala123 <obededintala.com>
2026-08-10 02:46:27 +0100

    fix: build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 
 export default defineConfig({
-  site: "https://obede.vercel.app",
   vite: {
     plugins: [tailwindcss()],
   },
